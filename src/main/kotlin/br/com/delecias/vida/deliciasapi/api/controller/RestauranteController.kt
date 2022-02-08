d159ff05cb5e62a65a11347ddd162e82764899f1
Joao Carlos Ferreira Marques <joaocarlusferrera@gmail.com>
2022-02-07 21:54:49 -0300

    4.31

--- src/main/kotlin/br/com/delecias/vida/deliciasapi/api/controller/RestauranteController.kt
+++ src/main/kotlin/br/com/delecias/vida/deliciasapi/api/controller/RestauranteController.kt
@@ -4,6 +4,7 @@ import br.com.delecias.vida.deliciasapi.domain.exception.EntidadeNaoEncontradaEx
 import br.com.delecias.vida.deliciasapi.domain.model.Restaurante
 import br.com.delecias.vida.deliciasapi.domain.repository.RestauranteRepository
 import br.com.delecias.vida.deliciasapi.domain.service.CadastroRestauranteService
+import org.springframework.beans.BeanUtils
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -30,11 +31,29 @@ class RestauranteController(
     }
 
     @PostMapping
-    fun cadastrarRestaurante(@RequestBody restaurante: Restaurante) : ResponseEntity<Any>{
-        return try{
+    fun cadastrarRestaurante(@RequestBody restaurante: Restaurante): ResponseEntity<Any> {
+        return try {
             val restauranteCadastrado = serviceRestaurante.salvar(restaurante)
             ResponseEntity.status(HttpStatus.CREATED).body(restauranteCadastrado)
-        }catch (e : EntidadeNaoEncontradaException){
+        } catch (e: EntidadeNaoEncontradaException) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
+        }
+    }
+
+    @PutMapping(value = ["/{restauranteId}"])
+    fun putRestaurante(
+        @PathVariable("restauranteId") restauranteId: Long,
+        @RequestBody restaurante: Restaurante
+    ): ResponseEntity<Any> {
+
+        val restauranteAtual = repoRestaurante.buscar(restauranteId) ?: return ResponseEntity.notFound().build()
+
+        BeanUtils.copyProperties(restaurante, restauranteAtual, "id")
+
+        return try {
+            val restauranteAtualizado = serviceRestaurante.salvar(restauranteAtual)
+            return ResponseEntity.ok(restauranteAtualizado)
+        } catch (e: EntidadeNaoEncontradaException) {
             ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
         }
     }
